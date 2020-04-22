3cd71e9c043fa1bb219f8c14bfbeed6f3f0b1e1a
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-04-22 15:43:18 +0300

    exercise1

---
+++ week1/week1Homework/week1/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
+
+
+function Hobby(props) {
+  return <li>{props.name}</li>;
+}
+
+function HobbyList() {
+  const [hobby, setHobby] = useState(hobbies);
+  return <ul>{hobbies.map(item => {
+    return <Hobby name={item} />
+  })}</ul>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <HobbyList />
+    </div>
+  );
+}
+
+export default App;
