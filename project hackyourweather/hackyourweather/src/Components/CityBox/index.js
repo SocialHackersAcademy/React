d24a2016564f0853ee8359e159ceb4cffff2f57e
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-04-24 01:26:28 +0300

    added some css

--- project hackyourweather/hackyourweather/src/Components/CityBox/index.js
+++ project hackyourweather/hackyourweather/src/Components/CityBox/index.js
@@ -4,6 +4,11 @@ import './styles.css'
 
 function CityBox(props) {
     const city = props.data
+    const minTemp = (city.main.temp_min - 273.15).toFixed(1)
+    const maxTemp = (city.main.temp_max - 273.15).toFixed(1)
+
+
+
     return (
         <div className="cityBox">
             <h1>{city.name} , {city.sys.country}</h1>
@@ -11,8 +16,8 @@ function CityBox(props) {
                 <h3>{city.weather[0]['main']}    </h3>
                 <h4>{city.weather[0]['description']}</h4>
             </div>
-            <p>min temp : {city.main.temp_min}</p>
-            <p>max temp :{city.main.temp_max}</p>
+            <p>min temp : {minTemp} °C</p>
+            <p>max temp :{maxTemp} °C</p>
             <p>location :{city.coord.lat}, {city.coord.lon}</p>
         </div>
     )
